35ea5638ad1f5f7bbddf728cce52cf2f6f362bbb
zer0ken <a37592777@gmail.com>
2024-12-04 03:06:40 +0900

    Add graph image generation to main.py and include graph.png file

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ load_dotenv()
 from graph.graph import graph, stream_graph_updates
 
 if __name__ == '__main__':
-    
-    graph.get_graph().print_ascii()
+    from IPython.display import Image, display
+    try:
+        graph_img = Image(graph.get_graph().draw_mermaid_png())
+        open('graph.png', 'wb').write(graph_img.data)
+    except Exception:
+        pass
     
     while True:
         try:
