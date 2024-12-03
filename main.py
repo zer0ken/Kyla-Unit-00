2a4420a8f2a03370363b8415ef72c78d7a3074a3
zer0ken <a37592777@gmail.com>
2024-12-04 03:07:24 +0900

    Remove unused import from main.py to streamline code and improve readability.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ load_dotenv()
 from graph.graph import graph, stream_graph_updates
 
 if __name__ == '__main__':
-    from IPython.display import Image, display
+    from IPython.display import Image
     try:
         graph_img = Image(graph.get_graph().draw_mermaid_png())
         open('graph.png', 'wb').write(graph_img.data)
