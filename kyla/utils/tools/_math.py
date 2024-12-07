052873e9dcb0f8965ddd572d2bcf939f67518dc1
zer0ken <a37592777@gmail.com>
2024-12-08 01:01:38 +0900

    Enhance mathematical toolset by adding power and rounding functions. Update div function to return a tuple of quotient and remainder. This improves functionality and expands the capabilities of the tools available for mathematical operations.

--- kyla/utils/tools/_math.py
+++ kyla/utils/tools/_math.py
@@ -1,3 +1,4 @@
+from typing import Tuple, Union
 from langchain_core.tools import tool
 
 
@@ -38,7 +39,7 @@ def mul(a: int, b: int) -> int:
 
 
 @tool
-def div(a: int, b: int, div_and_mod: bool = False) -> int:
+def div(a: int, b: int, div_and_mod: bool = False) -> Union[int, Tuple[int, int]]:
     """
     첫 번째 수를 두 번째 수로 나눕니다.
 
@@ -52,4 +53,24 @@ def div(a: int, b: int, div_and_mod: bool = False) -> int:
     return a / b
 
 
-tools = [add, sub, mul, div] 
+@tool
+def pow(a: int, b: int) -> int:
+    """
+    첫 번째 수를 두 번째 수만큼 거듭제곱합니다.
+
+    Args:
+        a: 첫 번째 수
+        b: 두 번째 수
+    """
+    return a ** b
+
+
+@tool
+def round(a: float) -> int:
+    """
+    소수점을 반올림합니다.
+    """
+    return round(a)
+
+
+tools = [add, sub, mul, div, pow, round]
